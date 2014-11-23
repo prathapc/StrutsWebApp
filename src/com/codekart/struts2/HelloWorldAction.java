197e9f4140f3cb1434557f13aeec81805fcb4e44
prathapc <prathap.choudhury@gmail.com>
2014-11-23 23:53:18 +0530

    first commit:helloworld wrkg fine

---
+++ src/com/codekart/struts2/HelloWorldAction.java
@@ -0,0 +1,17 @@
+package com.codekart.struts2;
+
+public class HelloWorldAction {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String execute() throws Exception {
+		return "success";
+	}
+}
